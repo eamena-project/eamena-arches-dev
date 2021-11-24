5da761738fe15b6e862ca8112245e5be435a47c3
zoometh <thomashuet7@gmail.com>
2021-11-24 17:40:27 +0000

    bis

--- select/select_all_jordan.sql
+++ select/select_all_jordan.sql
@@ -1,5 +1,5 @@
 SELECT nodes.nodeid, geojson_geometries.geom
 FROM nodes, geojson_geometries
 WHERE nodes.nodeid = geojson_geometries.nodeid
-AND ST_Overlaps( JORDAN_GEOM, geojson_geometries.geom)
+AND ST_Overlaps(JORDAN_GEOM, geojson_geometries.geom)
 -- LIMIT 20

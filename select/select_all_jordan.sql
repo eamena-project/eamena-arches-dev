1aebfe24986912c41e94244246eca099e1dcb86b
zoometh <thomashuet7@gmail.com>
2021-12-02 17:55:19 +0000

    doc

--- select/select_all_jordan.sql
+++ select/select_all_jordan.sql
@@ -1,5 +1,7 @@
--- select a sample of Jordan data
--- by Galen Mancino
+-- select a sample of Jordan data (by Galen Mancino)
+-- prepared to:
+--              - select all data from a given region
+--              - only a sample of data from a given region
 select resource_instances.resourceinstanceid, resource_x_resource.resourceinstanceidfrom, resource_instances.graphid from resource_instances
 inner join resource_x_resource on resource_instances.resourceinstanceid = resource_x_resource.resourceinstanceidfrom
 where resource_instances.graphid = '81212939-f98a-11e9-b345-06f597a7d5ce'

f5f6da0c59487cbe7e697fbdea91c7a52e66c314
zoometh <thomashuet7@gmail.com>
2021-12-15 18:05:04 +0000

    connect Pg EAMENA

---
+++ select/_report_annual_21.R
@@ -0,0 +1,19 @@
+library(RPostgreSQL)
+library(DBI)
+
+source(paste0(getwd(), "/select/select.R")) # read the SQL queries
+
+drv <- dbDriver("PostgreSQL")
+
+con <- dbConnect(drv,
+                 user = 'postgres',
+                 password = 'postgis',
+                 dbname = 'eamena',
+                 host = 'ec2-54-155-109-226.eu-west-1.compute.amazonaws.com',
+                 port = 5432)
+# dbListTables(con)
+HPs_count <- dbGetQuery(con, d.sql[["HPs_count"]])
+
+HPs_count # count of HPs
+
+dbDisconnect(con)

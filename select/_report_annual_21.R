53d0c24dc9538cb775a976a9dd28e705c45c774a
zoometh <thomashuet7@gmail.com>
2022-01-17 15:55:57 +0000

    Budat

--- select/_report_annual_21.R
+++ select/_report_annual_21.R
@@ -6,8 +6,8 @@ source(paste0(getwd(), "/select/select.R")) # read the SQL queries
 drv <- dbDriver("PostgreSQL")
 
 con <- dbConnect(drv,
-                 user = 'xxx',
-                 password = 'xxx',
+                 user = 'postgres',
+                 password = 'postgis',
                  dbname = 'eamena',
                  host = 'ec2-54-155-109-226.eu-west-1.compute.amazonaws.com',
                  port = 5432)

5f5129e04fa8555664408524d4b226c6d336f84b
zoometh <thomashuet7@gmail.com>
2022-01-21 18:08:26 +0000

    APP ROOT

---
+++ training/commands.sh
@@ -0,0 +1,21 @@
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## main Linux commands
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cd      # change directory
+ls      # list folder and files
+pwd     # current directory
+
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## use environmental variable to navigate into the folders
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# add the project name in a 'permanent' environmental variable
+cd /etc
+sudo vim environment
+# replace 'xxx' by the country name &
+# add line: project_name="xxxx_project"
+
+# use $project_name to cd to the root of the project (APP_ROOT)
+/home/archesadmin/$project_name/$project_name/
+
+

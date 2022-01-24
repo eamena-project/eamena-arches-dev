b926325a7e40d9d28e5447f4ec0314909504f4b1
zoometh <thomashuet7@gmail.com>
2022-01-24 15:02:50 +0000

    bis

--- training/JordanMasdar/files/settings_local.py
+++ training/JordanMasdar/files/settings_local.py
@@ -2,3 +2,35 @@ try:
     from .jordan_project.settings import *
 except ImportError:
     pass
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## my private variables
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# AWS_ACCESS_KEY_ID = 'AKIAxxxxxxxxxxxxxxxx'
+# AWS_SECRET_ACCESS_KEY = 'IKXame/xxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+# BING_KEY = "AoNIucpU5Ivxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# DATABASES = {
+#     "default": {
+#         "ATOMIC_REQUESTS": False,
+#         "AUTOCOMMIT": True,
+#         "CONN_MAX_AGE": 0,
+#         "ENGINE": "django.contrib.gis.db.backends.postgis",
+#         "HOST": "localhost",
+#         "NAME": "jordan_project",
+#         "OPTIONS": {},
+#         "PASSWORD": "postgis",
+#         "PORT": "5432",
+#         "POSTGIS_TEMPLATE": "template_postgis",
+#         "TEST": {
+#             "CHARSET": None,
+#             "COLLATION": None,
+#             "MIRROR": None,
+#             "NAME": None
+#         },
+#         "TIME_ZONE": None,
+#         "USER": "postgres"
+#     }
+# }

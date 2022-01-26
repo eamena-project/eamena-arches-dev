879a0680261557789a9f9c891dffd4dbac587f95
zoometh <thomashuet7@gmail.com>
2022-01-26 18:05:49 +0000

    header

--- training/commands.sh
+++ training/commands.sh
@@ -1,3 +1,10 @@
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -##
+# Welcome to Aches/EAMENA Database Manager training !          #
+# This document resume some IT commands                        #
+#                               credit: University of Oxford   #
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## main Linux commands
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -21,20 +28,20 @@ sudo vim environment
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # replace $project_name by the name of your project
 # root of the project (APP_ROOT)
-cd /home/archesadmin/$project_name/$project_name    # PuTTY
-/home/archesadmin/$project_name/$project_name       # FileZilla (Remote site)
+cd /home/archesadmin/$project_name/$project_name                                # PuTTY
+/home/archesadmin/$project_name/$project_name                                   # FileZilla (Remote site)
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## main configuration files
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # settings.py and settings_local.py
-cd /home/archesadmin/$project_name/$project_name    # PuTTY
-/home/archesadmin/$project_name/$project_name       # FileZilla (Remote site)
+cd /home/archesadmin/$project_name/$project_name                                # PuTTY
+/home/archesadmin/$project_name/$project_name                                   # FileZilla (Remote site)
 
 # 000-default.conf
-cd /etc/apache2/sites-available                     # PuTTY
-/etc/apache2/sites-available                        # FileZilla (Remote site)
+cd /etc/apache2/sites-available                                                 # PuTTY
+/etc/apache2/sites-available                                                    # FileZilla (Remote site)
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## understanding Python
@@ -43,9 +50,9 @@ ubuntu@ip-172-31-46-153:~$ cd /home/archesadmin/
 # activate Python virtual environment
 ubuntu@ip-172-31-46-153:/home/archesadmin$ source env/bin/activate
 # got to settings.py directory
-(env) ubuntu@ip-172-31-46-153:/home/archesadmin$ cd jordan_project/jordan_project/
+(env) ubuntu@ip-172-31-46-153:/home/archesadmin$ cd $project_name/$project_name/
 # run Python
-(env) ubuntu@ip-172-31-46-153:/home/archesadmin/jordan_project/jordan_project$ python
+(env) ubuntu@ip-172-31-46-153:/home/archesadmin/$project_name/$project_name$ python
 Python 3.8.10 (default, Nov 26 2021, 20:14:08)
 [GCC 9.3.0] on linux
 Type "help", "copyright", "credits" or "license" for more information.
@@ -58,9 +65,9 @@ ubuntu@ip-172-31-46-153:~$ cd /home/archesadmin/
 # activate Python virtual environment
 ubuntu@ip-172-31-46-153:/home/archesadmin$ source env/bin/activate
 # got to settings.py directory
-(env) ubuntu@ip-172-31-46-153:/home/archesadmin$ cd jordan_project/jordan_project/
+(env) ubuntu@ip-172-31-46-153:/home/archesadmin$ cd $project_name/$project_name/
 # run Python
-(env) ubuntu@ip-172-31-46-153:/home/archesadmin/jordan_project/jordan_project$ python
+(env) ubuntu@ip-172-31-46-153:/home/archesadmin/$project_name/$project_name$ python
 Python 3.8.10 (default, Nov 26 2021, 20:14:08)
 [GCC 9.3.0] on linux
 Type "help", "copyright", "credits" or "license" for more information.
@@ -68,8 +75,15 @@ Type "help", "copyright", "credits" or "license" for more information.
 # APP_ROOT (https://github.com/eamena-oxford/eamena-arches-dev/blob/f05af3a8d851bed41724e1717d1d986a93b72a65/training/JordanMasdar/files/settings.py#L16)
 >>> APP_ROOT = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 >>> print(APP_ROOT)
-/home/archesadmin/jordan_project/jordan_project
-# the APP_ROOT is '/home/archesadmin/jordan_project/jordan_project'
+/home/archesadmin/$project_name/$project_name
+# the APP_ROOT is '/home/archesadmin/$project_name/$project_name'
 # >>> MEDIA_URL = '/files/'
 # >>> MEDIA_ROOT =  os.path.join(APP_ROOT)
 # >>> print(MEDIA_ROOT)
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## understanding APP_ROOT
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# move to the landing/ folder
+/home/archesadmin/$project_name/$project_name/static/img/                       # FileZilla (Remote site)
+

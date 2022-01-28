ea97feeb0ab7deadd968a003a9a2576c35fae5bc
zoometh <thomashuet7@gmail.com>
2022-01-28 10:53:59 +0000

    cmd

--- training/commands.sh
+++ training/commands.sh
@@ -1,8 +1,10 @@
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -##
-# Welcome to the Aches/EAMENA Database Manager training !      #
-# This document resume some IT commands                        #
-#                               credit: University of Oxford   #
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ##
+# Welcome to the Aches/EAMENA Database Manager training !                         #
+# This document resume some IT commands following the install of Arches:          #
+#  https://github.com/zoometh/arches-scripts/blob/master/install_and_apache.sh    #
+#                                                  credit: University of Oxford   #
+#                                                  year  :                 2022   #
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ##
 
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -16,43 +18,81 @@ ls -la                        # list folder and files + permissions + owners (hi
 man command_name              # command documentation
 pwd                           # current directory
 source                        # read and execute the content of a file
+sudo                          # do as superuser
 su user_name                  # change user
-
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-## Ubuntu: understanding permissions and ownerships
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# add the project name in a 'permanent' environmental variable
-cd /home/archesadmin/jordan_project/jordan_project/static/img
-# list files and folders permissions and ownerships
-ls -la
-# ...
-# drwxrwxr-x  4 archesadmin www-data  4096 Jan 17 11:59 landing
-# ..
-su archesadmin
-
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Ubuntu: use of environmental variables
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# add the project name in a 'permanent' environmental variable
+# add 'permanent' environmental variables                                       # PuTTY
+
+# move to etc/ folder
 cd /etc
+
+# edit 'environment' file with vim (sudo mode)
 sudo vim environment
-# replace 'xxx' by the country name &
-# add line: project_name="xxxx_project"
+# insert  (ESC + I) the following variables:
+# your project name, replace 'xxxx' by your country name
+project_name="xxxx_project"
+# your Arches user name (app admin)
+username="archesadmin"
+# save and quit
+
+# make environment variables visibles (excecutable) by anyone
+sudo chmod 745 environment
+
+# check out
+# call the variables (echo)
+echo $username
+# .. gives: archesadmin
+echo $project_name
+# .. gives: the name of your project
+cd /home/$username/$project_name/$project_name
+# .. moves to your Arches project root folder
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## Ubuntu: authorised a Arches user to connect via SSH
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# see current permissions of SSH authorized keys                                # PuTTY
+cd ~/.ssh
+ls -l
+# copy the authorized_keys file that contains your public key
+sudo cp /home/ubuntu/.ssh/authorized_keys /home/$username/.ssh/authorized_keys
+# move to this copy
+cd /home/$username/
+# change file ownership from ubuntu to Arches user
+sudo chown -R $username:$username /home/$username/.ssh
+
+# check out
+ls -al
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## Ubuntu: understanding permissions and ownerships
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# move to img/ folder
+cd /home/$username/$project_name/$project_name/static/img
+# list files' and folders' permissions and ownerships
+ls -la
+# ...
+# drwxrwxr-x  4 archesadmin www-data  4096 Jan 17 11:59 landing
+# ..
+su $username
+
+
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # replace $project_name by the name of your project
 # root of the project (APP_ROOT)
-cd /home/archesadmin/$project_name/$project_name                                # PuTTY
-/home/archesadmin/$project_name/$project_name                                   # FileZilla (Remote site)
+cd /home/$username/$project_name/$project_name                                # PuTTY
+/home/$username/$project_name/$project_name                                   # FileZilla (Remote site)
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## main configuration files
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # settings.py and settings_local.py
-cd /home/archesadmin/$project_name/$project_name                                # PuTTY
-/home/archesadmin/$project_name/$project_name                                   # FileZilla (Remote site)
+cd /home/$username/$project_name/$project_name                                # PuTTY
+/home/$username/$project_name/$project_name                                   # FileZilla (Remote site)
 
 # 000-default.conf
 cd /etc/apache2/sites-available                                                 # PuTTY
@@ -61,7 +101,7 @@ cd /etc/apache2/sites-available                                                 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## understanding Python
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-ubuntu@ip-172-31-46-153:~$ cd /home/archesadmin/
+ubuntu@ip-172-31-46-153:~$ cd /home/$username/
 # activate Python virtual environment
 ubuntu@ip-172-31-46-153:/home/archesadmin$ source env/bin/activate
 # got to settings.py directory
@@ -76,7 +116,7 @@ Type "help", "copyright", "credits" or "license" for more information.
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## understanding APP_ROOT
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-ubuntu@ip-172-31-46-153:~$ cd /home/archesadmin
+ubuntu@ip-172-31-46-153:~$ cd /home/$username
 # activate Python virtual environment
 ubuntu@ip-172-31-46-153:/home/archesadmin$ source env/bin/activate
 # got to settings.py directory
@@ -90,7 +130,7 @@ Type "help", "copyright", "credits" or "license" for more information.
 # APP_ROOT (https://github.com/eamena-oxford/eamena-arches-dev/blob/f05af3a8d851bed41724e1717d1d986a93b72a65/training/JordanMasdar/files/settings.py#L16)
 >>> APP_ROOT = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 >>> print(APP_ROOT)
-/home/archesadmin/$project_name/$project_name
+/home/$username/$project_name/$project_name
 # the APP_ROOT is '/home/archesadmin/$project_name/$project_name'
 # >>> MEDIA_URL = '/files/'
 # >>> MEDIA_ROOT =  os.path.join(APP_ROOT)
@@ -99,9 +139,11 @@ Type "help", "copyright", "credits" or "license" for more information.
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## customize homepage of the Arches/EAMENA
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
 # move to the landing/ project folder
-/home/archesadmin/$project_name/$project_name/static/img/landing                # FileZilla (Remote site)
+/home/$username/$project_name/$project_name/static/img/landing                # FileZilla (Remote site)
 
 # move to the templates/ project folder
-/home/archesadmin/$project_name/$project_name/templates                         # FileZilla (Remote site)
+/home/$username/$project_name/$project_name/templates                         # FileZilla (Remote site)
 

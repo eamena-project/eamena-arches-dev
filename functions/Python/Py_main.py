2940185306f69d9f60826c7c74080d0c94cbf773
zoometh <thomashuet7@gmail.com>
2022-06-20 16:08:57 +0100

    python docstrings

--- functions/Python/Py_main.py
+++ functions/Python/Py_main.py
@@ -9,7 +9,8 @@ my_uuid = get_uuid_from_eaid('EAMENA-0181523')
 # my_geom = get_geom_from_uuid(my_uuid)
 # my_related_resources = get_related_resources_from_uuid(my_uuid)
 my_geom = get_geom_from_uuid('2a774cbd-d13f-41ce-8af1-292344bd4dff', 'bc')
-export_geom_from_urlgeojson(out_geom_name = "new_new_new_geojson")
+print(my_geom)
+# export_geom_from_urlgeojson(out_geom_name = "new_new_new_geojson")
 
 
 # - - - - - - - - - - - - -

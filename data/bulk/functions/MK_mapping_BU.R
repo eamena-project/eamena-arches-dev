4cbef24801657a5862630e0eb47abd2ba3241def
zoometh <thomashuet7@gmail.com>
2022-07-15 11:16:37 +0100

    first eval(parse))

--- data/bulk/functions/MK_mapping_BU.R
+++ data/bulk/functions/MK_mapping_BU.R
@@ -8,6 +8,7 @@ library(googlesheets4)
 #   - MK field one-to-one correspondences ('field');
 #   - constant values ('value');
 #   - logical functions ('expression');
+#   - depending values ('escape') not read, dependent from another column evaluated by 'expression'
 #
 # mapping file: https://docs.google.com/spreadsheets/d/1nXgz98mGOySgc0Q2zIeT1RvHGNl4WRq1Fp9m5qB8g8k/edit#gid=1083097625
 # export a TSV here: https://github.com/eamena-oxford/eamena-arches-dev/blob/main/data/bulk/examples/
@@ -55,8 +56,98 @@ for(i in seq(1, nrow(mapping.file.value))){
 
 # 'expression'
 # TODO
+mapping.file.expres <- mapping.file[mapping.file$type == "expression", ]
+# mapping.file.expres[, "EAMENA"]
+# for(i in seq(1, nrow(mapping.file.expres))){
+#   # i <- 1
+#   ea <- as.character(mapping.file.expres[i, "EAMENA"])
+#   x.text <- as.character(mapping.file.expres[i, "MKdone"])
+#   x <- eval(parse(text = x.text)) # the text is executed as a function
+#   # x <- as.character(mapping.file.expres[i, "MKdone"])
+#   bu[ , ea] <- x
+# }
+for(i in seq(1, nrow(mapping.file.expres))){
+  # i <- 1
+  ea <- as.character(mapping.file.expres[i, "EAMENA"])
+  x.text <- as.character(mapping.file.expres[i, "MKdone"])
+  eval(parse(text = x.text)) # the XLSX cell text is executed
+}
 
 # export
 write.table(bu, paste0(bu.path, "examples/", Sys.Date(), "-Mohamed.tsv"),
             row.names = F,
             sep = "\t")
+
+# - - - - - - - - -
+
+
+# # Cultural.Period.Type
+# for(j in seq(1, nrow(mk.data))){
+#   Classical <- mk.data[j, "Roman"] == "Y" | mk.data[j, "Byzantine"] == "Y"
+#   if(!is.na(Classical)){
+#     bu[j, ea] <- "Classical/Protohistoric/Pre-Islamic (North Africa)"
+#     bu[j, "Cultural.Period.Certainty"] <- "Possible"
+#   }
+#   Berber <- mk.data[j, "Breber"] == "Y" | mk.data[j, "Berber for"] == "Y"
+#   if(!is.na(Classical)){
+#     bu[j, ea] <- "Unknown"
+#     bu[j, "Cultural.Period.Certainty"] <- "Not Applicable"
+#   }
+# }
+
+
+
+# Heritage.Place.Function
+# for(j in seq(1, nrow(mk.data))){
+#   Funerary <- mk.data[j, "Tomb"] == "Y" | mk.data[j, "Mausoleum"] == "Y" | mk.data[j, "Bazina"] == "Y"
+#   if(!is.na(Funerary)){
+#     bu[j, ea] <- "Funerary/Memorial"
+#     bu[j, "Heritage.Place.Function.Certainty"] <- "Medium"
+#   }
+#   Domestic <- mk.data[j, "Town"] == "Y" | mk.data[j, "Village"] == "Y"
+#   if(!is.na(Domestic)){
+#     bu[j, ea] <- "Domestic"
+#     bu[j, "Heritage.Place.Function.Certainty"] <- "Medium"
+#   }
+#   Agricultural <- mk.data[j, "Farm.Site"] == "Y"
+#   if(!is.na(Agricultural)){
+#     bu[j, ea] <- "Agricultural/Pastoral"
+#     bu[j, "Heritage.Place.Function.Certainty"] <- "Medium"
+#   }
+#   Industrial <- mk.data[j, "Press"] == "Y"
+#   if(!is.na(Industrial)){
+#     bu[j, ea] <- "Industrial/Productive"
+#     bu[j, "Heritage.Place.Function.Certainty"] <- "Medium"
+#   }
+#   Hydrological <- mk.data[j, "Aqueduct"] == "Y"
+#   if(!is.na(Hydrological)){
+#     bu[j, ea] <- "Hydrological"
+#     bu[j, "Heritage.Place.Function.Certainty"] <- "Medium"
+#   }
+#   Religious <- mk.data[j, "Church"] == "Y"
+#   if(!is.na(Religious)){
+#     bu[j, ea] <- "Religious"
+#     bu[j, "Heritage.Place.Function.Certainty"] <- "Medium"
+#   }
+#   Defensive <- mk.data[j, "Roman.Fort"] == "Y" | mk.data[j, "Fort.gasr"] == "Y"
+#   if(!is.na(Defensive)){
+#     bu[j, ea] <- "Defensive/Fortification"
+#     bu[j, "Heritage.Place.Function.Certainty"] <- "Medium"
+#   }
+#   # - - - - - - - - - - - - - - - - -
+#   Structure <- Funerary | Domestic | Agricultural | Industrial | Hydrological | Religious | Defensive
+#   if(!is.na(Structure)){
+#     bu[j, "Site.Feature.Form.Type"] <- "Structure"
+#     bu[j, "Site.Feature.Form.Type.Certainty"] <- "Medium"
+#   }
+# }
+
+
+
+
+
+
+
+
+
+

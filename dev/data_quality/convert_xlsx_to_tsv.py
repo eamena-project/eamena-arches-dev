9468cec619e4cd46a913d11e68325fdaa34eb700
zoometh <thomashuet7@gmail.com>
2023-09-29 17:15:19 +0100

    action3

--- dev/data_quality/convert_xlsx_to_tsv.py
+++ dev/data_quality/convert_xlsx_to_tsv.py
@@ -1,20 +1,15 @@
 import openpyxl
 import csv
 
-# Replace 'input.xlsx' with the actual name of your XLSX file
-# Replace 'output.tsv' with the desired name of your TSV file
-xlsx_filename = 'template.xlsx'
-tsv_filename = 'template.tsv'
+xlsx_filename = 'dev/data_quality/template.xlsx'
+tsv_filename = 'dev/data_quality/output.tsv'
 
-# Load the XLSX file
 wb = openpyxl.load_workbook(xlsx_filename)
 sheet = wb.active
 
-# Open the TSV file for writing
 with open(tsv_filename, 'w', newline='') as tsv_file:
     tsv_writer = csv.writer(tsv_file, delimiter='\t')
 
-    # Iterate through rows in the XLSX sheet and write them as TSV rows
     for row in sheet.iter_rows(values_only=True):
         tsv_writer.writerow(row)
 

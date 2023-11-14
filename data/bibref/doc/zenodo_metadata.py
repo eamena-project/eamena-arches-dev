2cea67fc3d45746f508f747bb3c5afa56d9acf67
zoometh <thomashuet7@gmail.com>
2023-11-14 17:12:07 +0000

    zenodo metdata

--- data/bibref/doc/zenodo_metadata.py
+++ data/bibref/doc/zenodo_metadata.py
@@ -19,79 +19,62 @@ data = resp.json()
 
 #%%
 
-def field_values(fieldname = None):
-    l = list()
-    for i in range(len(data['features'])):
-        l.append(data['features'][i]['properties'][fieldname])
-    return(l)
+# def field_values(fieldname = None):
+#     l = list()
+#     for i in range(len(data['features'])):
+#         l.append(data['features'][i]['properties'][fieldname])
+#     return(l)
 
-def summed_values(l = None):
-    # split_names = [name.strip() for item in l for name in item.split(',')]
-    split_names = [name.strip() for item in l if item is not None for name in item.split(',')]
-    name_counts = Counter(split_names)
-    df = pd.DataFrame.from_dict(name_counts, orient='index').reset_index()
-    df = df.rename(columns={'index': 'name', 0: 'n_hp'})
-    df = df.sort_values('n_hp', ascending=False)
-    return df
+# def summed_values(l = None):
+#     # split_names = [name.strip() for item in l for name in item.split(',')]
+#     split_names = [name.strip() for item in l if item is not None for name in item.split(',')]
+#     name_counts = Counter(split_names)
+#     df = pd.DataFrame.from_dict(name_counts, orient='index').reset_index()
+#     df = df.rename(columns={'index': 'name', 0: 'n_hp'})
+#     df = df.sort_values('n_hp', ascending=False)
+#     return df
+
+# def zenodo_contributors(contributors_layout = {"name": None, "affiliation": None, "orcid": None}):
+#     l = field_values("Assessment Investigator - Actor")
+#     df = summed_values(l)
+#     CONTRIBUTORS = list()
+#     for name in df['name']:
+#         contributors_layout['name'] = name
+#         # TODO: "affiliation" and "orcid"
+#         CONTRIBUTORS.append(contributors_layout.copy())
+#     return CONTRIBUTORS
+
+# def zenodo_keywords(constant = ['EAMENA', 'MaREA'], others = ["Country Type", "Cultural Period Type"]):
+#     KEYWORDS = list()
+#     KEYWORDS = KEYWORDS + constant
+#     for i in others:
+#         l = field_values(i)
+#         df = summed_values(l)
+#         KEYWORDS = KEYWORDS + df['name'].tolist()
+#     KEYWORDS.remove('Unknown')
+#     return KEYWORDS
+
+# def zenodo_dates(fields = ["Assessment Activity Date"]):
+#     ldates = list()
+#     for i in fields:
+#         l = field_values("Assessment Activity Date")
+#         df = summed_values(l)
+#         ldates = ldates + df['name'].tolist() 
+#     ldates.remove('None')
+#     # date_strings = [x for x in date_strings if x is not 'None']
+#     date_objects = [datetime.strptime(date, '%Y-%m-%d') for date in ldates]
+#     min_date = min(date_objects)
+#     max_date = max(date_objects)
+#     min_date_str = min_date.strftime('%Y-%m-%d')
+#     max_date_str = max_date.strftime('%Y-%m-%d')
+#     DATES = [{'type': 'created', 'start': min_date_str, 'end': max_date_str}]
+#     return DATES
 
 #%%
 ## TITLE & DESCRIPTION = free text
 
 TITLE = "This is the title of my Zenodo deposit"
 DESCRIPTION = "This is the description of my Zenodo deposit"
-
-#%%
-## CONTRIBUTORS
-
-contributors_layout = {"name": None,
-                       "affiliation": None,
-                       "orcid": None}
-
-l = field_values("Assessment Investigator - Actor")
-df = summed_values(l)
-CONTRIBUTORS = list()
-# contributors_list = contributors_layout.copy()
-for name in df['name']:
-    contributors_layout['name'] = name
-    # TODO: "affiliation" and "orcid"
-    CONTRIBUTORS.append(contributors_layout.copy())
-
-#%%
-## KEYWORDS
-
-KEYWORDS = list()
-KEYWORDS.append('EAMENA')
-
-# Countries
-l = field_values("Country Type")
-df = summed_values(l)
-KEYWORDS = KEYWORDS + df['name'].tolist()
-
-# Periods
-l = field_values("Cultural Period Type")
-df = summed_values(l)
-KEYWORDS = KEYWORDS + df['name'].tolist()
-
-# remove keywords like 'Unknown'
-KEYWORDS.remove('Unknown')
-
-#%%
-## DATES
-
-l = field_values("Assessment Activity Date")
-df = summed_values(l)
-date_strings = df['name'].tolist()
-date_strings.remove('None')
-# date_strings = [x for x in date_strings if x is not 'None']
-date_objects = [datetime.strptime(date, '%Y-%m-%d') for date in date_strings]
-min_date = min(date_objects)
-max_date = max(date_objects)
-min_date_str = min_date.strftime('%Y-%m-%d')
-max_date_str = max_date.strftime('%Y-%m-%d')
-
-DATES = [{'type': 'created', 'start': min_date_str, 'end': max_date_str}]
-
-# %%
 
 metadata = {
      'metadata': {
@@ -100,13 +83,15 @@ metadata = {
          'description': DESCRIPTION,
          'creators': [{'name': "EAMENA database",
                        'affiliation': "University of Oxford, University of Southampton"}],
-         'contributors': CONTRIBUTORS,
+         'contributors': zenodo_contributors(),
          'license': 'cc-by',
-         'keywords': KEYWORDS,
-         'dates': DATES,
+         'keywords': zenodo_keywords(),
+         'dates': zenodo_dates(),
          'grants': [{'id': '051z6e826::4178'}]
      }
  }
 
 print(json.dumps(metadata, indent=4))
 
+
+# %%

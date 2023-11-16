15718970ff528d55eefcc58b740a6233e3e616c0
zoometh <thomashuet7@gmail.com>
2023-11-16 20:22:49 +0000

    add Koppen climate colors

--- data/layouts/colorramps/xlsx_to_md.py
+++ data/layouts/colorramps/xlsx_to_md.py
@@ -1,3 +1,7 @@
+
+
+#%%
+
 import os
 import pandas as pd
 
@@ -6,16 +10,16 @@ local = "C:/Rprojects/eamena-arches-dev/"
 
 # Load the XLSX file into a DataFrame
 xlsx_file = local + 'data/layouts/symbology.xlsx'
-df = pd.read_excel(xlsx_file)
+xls = pd.read_excel(xlsx_file, sheet_name=None)
 
-# Convert the DataFrame to a Markdown table
-markdown_table = df.to_markdown(index=False)
-
-# Specify the output Markdown file
-markdown_file = local + 'data/layouts/koppen.md'
-
-# Save the Markdown table to the specified file
-with open(markdown_file, 'w') as f:
-    f.write(markdown_table)
-
-print(f"Markdown table saved to {markdown_file}")
+for sheet_name, df in xls.items():
+    # Process each sheet as needed
+    print(f"Sheet Name: {sheet_name}")
+    # Convert the DataFrame to a Markdown table
+    markdown_table = df.to_markdown(index=False)
+    # Specify the output Markdown file
+    markdown_file = local + f'data/layouts/{sheet_name}.md'
+    # Save the Markdown table to the specified file
+    with open(markdown_file, 'w') as f:
+        f.write(markdown_table)
+    print(f"Markdown table saved to {markdown_file}")

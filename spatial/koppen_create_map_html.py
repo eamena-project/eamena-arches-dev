0160d4b1e448f9529f6715c787e7d1d9e2699bc8
zoometh <thomashuet7@gmail.com>
2023-11-24 13:56:01 +0000

    koppen HTML

--- spatial/koppen_create_map_html.py
+++ spatial/koppen_create_map_html.py
@@ -8,7 +8,7 @@ os.getcwd()
 import folium
 
 # Create a folium map centered at a specific location
-m = folium.Map(location=[20, 35], zoom_start=10)
+m = folium.Map(location=[35, 20], zoom_start=5)
 
 # Add WMS layer to the map
 wms_url = 'http://54.155.109.226:8080/geoserver/ows'  # Replace with your WMS server URL
@@ -17,7 +17,7 @@ wms_layer.add_to(m)
 
 # Add HTML title to the map
 title_html = """
-    <h3 align="center" style="font-size:16px">Koppen Climate Classification</h3>
+    <span align="center" style="font-size:16px">Koppen Climate Classification</span>
 	<a href="https://github.com/eamena-project/eamena-arches-dev/blob/main/data/layouts/koppen_climate_class.md">Legend</a>
     """
 m.get_root().html.add_child(folium.Element(title_html))

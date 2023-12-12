994bda993feb68774607f8aa051be6400e1c2ac3
zoometh <thomashuet7@gmail.com>
2023-12-12 19:13:32 +0000

    Google dev

--- data/gs_with_0_hp.py
+++ data/gs_with_0_hp.py
@@ -1,9 +1,6 @@
 # Grids with 0 Heritage places
 
-#%% 
 import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
-# import pandas as pd
 
 keys = "C:/Rprojects/eamena-arches-dev/data/keys/"
 gsheet_key = "gsheet-407918-65ebbb9cb656.json" # a key from the Google API platform
@@ -16,16 +13,9 @@ grid_square_values = []
 # Loop over each worksheet in the Google Sheet
 for worksheet in spreadsheet:
     print("Current Sheet:", worksheet.title)
-    # Get all records from the current worksheet as a list of dictionaries
     records = worksheet.get_all_records()
-    # Loop over each record in the worksheet
     for record in records:
-        # Check if 'Pins in GE' is 0 and collect 'Grid Square' values
         if record['Pins in GE'] == 0:
             grid_square_values.append(record['Grid Square'])
             
-# Print the final list of 'Grid Square' values
-len(grid_square_values)
 print("Grid Square values where 'Pins in GE' is 0:", grid_square_values)
-
-# %%

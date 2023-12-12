96ee6d94506fedb8490cd648eaf2f475ae07046a
zoometh <thomashuet7@gmail.com>
2023-12-12 19:03:44 +0000

    Google dev

--- data/gs_with_0_hp.py
+++ data/gs_with_0_hp.py
@@ -1,31 +1,31 @@
 # Grids with 0 Heritage places
 
+#%% 
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
+# from oauth2client.service_account import ServiceAccountCredentials
+# import pandas as pd
 
-# Set up Google Sheets API credentials
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('path/to/your/credentials.json', scope)
-client = gspread.authorize(creds)
+keys = "C:/Rprojects/eamena-arches-dev/data/keys/"
+gsheet_key = "gsheet-407918-65ebbb9cb656.json" # a key from the Google API platform
+gkey = keys + gsheet_key
 
-# Open the Google Sheet by URL
-url = 'https://docs.google.com/spreadsheets/d/1r2VLLiyJMaCl8l7C4sKilQ4ZMqEfAwhQAwYC7wGzmk0/edit#gid=1142334803'
-spreadsheet = client.open_by_url(url)
+gc = gspread.service_account(filename=gkey) # Google Client
+#%% 
 
-# Create an empty list to store 'Grid Square' values
+spreadsheet = gc.open("EAMENA Final Grid Squares")
 grid_square_values = []
 
 # Loop over each worksheet in the Google Sheet
-for worksheet in spreadsheet.worksheets():
-    # Get the worksheet data as a DataFrame
-    df = pd.DataFrame(worksheet.get_all_records())
-
-    # Filter rows where 'Pins in GE' is 0 and get 'Grid Square' values
-    filtered_data = df[df['Pins in GE'] == 0]['Grid Square'].tolist()
-
-    # Extend the list with the values from the current worksheet
-    grid_square_values.extend(filtered_data)
-
+for worksheet in spreadsheet:
+    print("Current Sheet:", worksheet.title)
+    # Get all records from the current worksheet as a list of dictionaries
+    records = worksheet.get_all_records()
+    # Loop over each record in the worksheet
+    for record in records:
+        # Check if 'Pins in GE' is 0 and collect 'Grid Square' values
+        if record['Pins in GE'] == 0:
+            grid_square_values.append(record['Grid Square'])
+            
 # Print the final list of 'Grid Square' values
 print("Grid Square values where 'Pins in GE' is 0:", grid_square_values)
+

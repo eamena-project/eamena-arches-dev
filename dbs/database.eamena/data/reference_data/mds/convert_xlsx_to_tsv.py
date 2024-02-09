416b4adca8de7e8416bd7d0d82f23bdf0a47d22e
zoometh <thomashuet7@gmail.com>
2024-02-09 09:51:41 +0000

    mds color function

--- dbs/database.eamena/data/reference_data/mds/convert_xlsx_to_tsv.py
+++ dbs/database.eamena/data/reference_data/mds/convert_xlsx_to_tsv.py
@@ -15,8 +15,13 @@ sheet = wb.active
 
 with open(tsv_filename, 'w', newline='') as tsv_file:
     tsv_writer = csv.writer(tsv_file, delimiter='\t')
-
     for row in sheet.iter_rows(values_only=True):
         tsv_writer.writerow(row)
 
 print(f'Conversion from XLSX to TSV complete. TSV file saved as {tsv_filename}')
+
+# #%%
+# import pandas as pd
+# df1=pd.read_csv("https://raw.githubusercontent.com/tuyenhavan/Statistics/Dataset/World_Life_Expectancy.csv",sep=";")
+
+# df1.head()

a47fd456aa7c4da8a764e098821f91b91153bde7
zoometh <thomashuet7@gmail.com>
2024-02-20 17:33:47 +0000

    pres

--- dbs/database.eamena/data/reference_data/mds/field-to-tsv.R
+++ dbs/database.eamena/data/reference_data/mds/field-to-tsv.R
@@ -16,18 +16,17 @@ df_filtered$color <- sub("^(.{7}).*", "\\1", df_filtered$color)
 df_filtered <- df_filtered[!duplicated(df_filtered), ]
 colnames(df_filtered)[colnames(df_filtered) == 'level1'] <- 'Groups'
 df_filtered$Groups <- stringr::str_to_title(df_filtered$Groups)
-
 # Apply the color and hyperlink together
 df_filtered$Groups <- mapply(function(group, color) {
   # sprintf("<a href='%s%s.tsv' target='_blank' style='background-color:%s; padding: 5px; font-size: 20px;'>%s</a>", 
   #         base_url, gsub(" ", "%20", group), color, group)
   sprintf("<a href='%s%s.tsv' target='_blank' style='font-size: 20px; font-family:arial; color:%s'>%s</a>", 
-          base_url, gsub(" ", "%20", group), color, group)
+          base_url, gsub(" ", "%20", group), color, stringr::str_to_upper(group))
 }, df_filtered$Groups, df_filtered$color)
-
+# df_filtered$Groups <- stringr::str_to_upper(df_filtered$Groups)
 dt_widget <- datatable(df_filtered[ , c("Groups"), drop=FALSE], escape = FALSE,
                        rownames = FALSE,
                        options = list(paging = FALSE, searching = FALSE, info = FALSE, autoWidth = TRUE)) 
-# dt_widget
+dt_widget
 saveWidget(dt_widget, paste0(mds.path, '/field-to-tsv.html'), selfcontained = TRUE)
 

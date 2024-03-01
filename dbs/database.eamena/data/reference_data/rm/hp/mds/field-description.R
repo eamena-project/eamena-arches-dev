c48c4095fe484429816dc9d501096cfe1888ca24
zoometh <thomashuet7@gmail.com>
2024-03-01 10:40:15 +0000

    add EAMENA_fr.xml

--- dbs/database.eamena/data/reference_data/rm/hp/mds/field-description.R
+++ dbs/database.eamena/data/reference_data/rm/hp/mds/field-description.R
@@ -4,9 +4,21 @@ library(htmlwidgets)
 
 ## Generates a DT table of field names and field description from 'mds-template.xlsx'
 
-mds.path <- "C:/Rprojects/eamena-arches-dev/dbs/database.eamena/data/reference_data/mds/"
+# paths
+root.path <- "C:/Rprojects/eamena-arches-dev/dbs/database.eamena/data/reference_data/rm/hp/"
+mds.path <- paste0(root.path, "mds/")
+tsv.path <- paste0(root.path, "values/")
+# 
 mds.template <- paste0(mds.path, "mds-template.xlsx")
 df <- read_excel(mds.template, sheet = 1)
+
+# TODO: add URL links to TSV files, ex: 
+# https://github.com/eamena-project/eamena-arches-dev/tree/main/dbs/database.eamena/data/reference_data/rm/hp/values/Archaeological_Assessment/Absolute_Chronology.tsv
+df$level1.url <- stringr::str_to_title(gsub(" ", "_", df$level1))
+df$level3.url <- stringr::str_to_title(gsub("//", "_", gsub(" ", "_", df$level3)))
+sort(df$level3.url)
+
+
 df_filtered <- df[, c("num", "level1", "level2", "level3", "description", "color")]
 colnames(df_filtered)[colnames(df_filtered) == 'level3'] <- 'Heritage Place field'
 # rm the transparency from colors (ie, mds)
@@ -19,4 +31,5 @@ dt_widget <- datatable(df_filtered[ , c("num", "Heritage Place field","descripti
     columns = c("Heritage Place field"),
     backgroundColor = styleEqual(df_filtered[["Heritage Place field"]], df_filtered$color)
   )
-saveWidget(dt_widget, paste0(mds.path, '/fields-description-1.html'), selfcontained = TRUE)
+saveWidget(dt_widget, paste0(mds.path, '/fields-description-2.html'), selfcontained = TRUE)
+

000cd415e0441691ca8549439033585f5eb9a5ec
zoometh <thomashuet7@gmail.com>
2024-07-17 08:15:00 +0100

    varia

--- data/grids/create_html_map.py
+++ data/grids/create_html_map.py
@@ -1,7 +1,7 @@
 
 import folium
 
-m = folium.Map(location=[24, 34], zoom_start=4)
+m = folium.Map(location=[24, 34], zoom_start=3)
 folium.GeoJson(
     'C:/Rprojects/eamena-arches-dev/data/grids/EAMENA_Grid_contour.geojson',
     name='geojson'
